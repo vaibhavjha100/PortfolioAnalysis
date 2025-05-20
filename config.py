f8d8ab3eff4c03f4daf6f2f5cba6aef6e175153d
Vaibhav Jha <vaibhavjha100@gmail.com>
2025-05-20 18:11:30 +1000

    add configuration file for project setup

---
+++ config.py
@@ -0,0 +1,9 @@
+"""
+Configuration file for the project.
+"""
+
+import os
+
+ROOTDIR = os.path.join(os.path.expanduser("~"), "PythonProjects", "PortfolioAnalysis")
+RAWDATADIR = os.path.join(ROOTDIR, "Tradebooks", "Raw")
+DATADIR = os.path.join(ROOTDIR, "Tradebooks", "Processed")
