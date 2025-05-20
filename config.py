20ef1b19a5d5a6d85e3714e914a94c27911472b4
Vaibhav Jha <vaibhavjha100@gmail.com>
2025-05-20 18:48:18 +1000

    update config.py to define directory paths and add stock broker formats

--- config.py
+++ config.py
@@ -4,6 +4,12 @@ Configuration file for the project.
 
 import os
 
-ROOTDIR = os.path.join(os.path.expanduser("~"), "PythonProjects", "PortfolioAnalysis")
+# Defining directory paths
+ROOTDIR = os.path.join(os.path.expanduser("~"), "PycharmProjects", "PortfolioAnalysis")
 RAWDATADIR = os.path.join(ROOTDIR, "Tradebooks", "Raw")
 DATADIR = os.path.join(ROOTDIR, "Tradebooks", "Processed")
+
+# Defining formats for different stock brokers
+ZERODHA_FORMAT = ['symbol', 'isin', 'trade_date', 'exchange', 'segment', 'series',
+       'trade_type', 'auction', 'quantity', 'price', 'trade_id', 'order_id',
+       'order_execution_time']
