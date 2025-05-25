f29dae1d8b2ea85c587576e5e11377bf8764b6a8
Vaibhav Jha <vaibhavjha100@gmail.com>
2025-05-25 20:50:39 +1000

    Add standard format for stock broker data processing

--- config.py
+++ config.py
@@ -12,6 +12,7 @@ DATADIR = os.path.join(ROOTDIR, "Tradebooks", "Processed")
 EXCELDIR = os.path.join(ROOTDIR, "Excel")
 
 # Defining formats for different stock brokers
+STANDARD_FORMAT = ['time', 'date', 'ticker', 'exchange', 'quantity', 'price']
 ZERODHA_FORMAT = ['symbol', 'isin', 'trade_date', 'exchange', 'segment', 'series',
        'trade_type', 'auction', 'quantity', 'price', 'trade_id', 'order_id',
        'order_execution_time']
