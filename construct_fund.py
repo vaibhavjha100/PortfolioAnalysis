9ac986171a3f1e9018eb1dca3f225d06f1da8ba7
Vaibhav Jha <vaibhavjha100@gmail.com>
2025-05-27 11:53:37 +1000

    Tried to segregate, then failed

--- construct_fund.py
+++ construct_fund.py
@@ -11,6 +11,183 @@ from datetime import date
 import xlwings as xw
 import time
 import matplotlib.pyplot as plt
+import preprocessing as ppc
+
+def get_tradebook(*broker, start_date=None, end_date=date.today()):
+    """
+    Function to get the tradebook for a specific broker.
+    By default, start date is None and end date is today.
+    """
+    if not broker:
+        broker = ('zerodha',)
+    tb = pd.read_csv(os.path.join(cfg.DATADIR, "tradebook.csv"))
+    tb.set_index('time', inplace=True)
+    tb['date'] = pd.to_datetime(tb['date'])
+    # If start date is not provided, set it to the earliest date in the data
+    if start_date is None:
+        start_date = tb['date'].min()
+    start_date = pd.Timestamp(start_date)
+    end_date = pd.Timestamp(end_date)
+    # Filter the tradebook based on start and end dates
+    tb = tb[(tb['date'] >= start_date) & (tb['date'] <= end_date)]
+    # Convert start_date to string format YYYY-MM-DD
+    start_date = start_date.strftime('%Y-%m-%d')
+
+    return tb, start_date
+
+def get_stock_data(tickers, start_date=None, end_date=date.today()):
+    """
+    Function to get stock data for a list of tickers.
+    By default, start date is None and end date is today.
+    """
+    if start_date is None:
+        print("Function cannot be used without start date.")
+        return None
+    end_date = end_date.strftime('%Y-%m-%d')
+    stock_data = {}
+    ineligible_tickers = []
+    app = xw.App(visible=False)
+    wb = app.books.open(os.path.join(cfg.EXCELDIR, 'Stockdata.xlsx'))
+    sheet = wb.sheets[0]
+    start_year = start_date.split("-")[0]
+    start_month = start_date.split("-")[1]
+    start_day = start_date.split("-")[2]
+    end_year = end_date.split("-")[0]
+    end_month = end_date.split("-")[1]
+    end_day = end_date.split("-")[2]
+    for ticker in tickers:
+        # Clear the sheet before writing new data
+        sheet.clear()
+        cell = sheet.range("A1")
+        cell.value = ticker
+        cell2 = sheet.range("A2")
+        cell2.formula2 = f'=STOCKHISTORY(A1, DATE({start_year}, {start_month}, {start_day}), DATE({end_year}, {end_month}, {end_day}), 0)'
+        # Sleep for 5 seconds to allow the formula to calculate
+        time.sleep(2.5)
+        # Get all the data from the sheet into a dataframe
+        data = sheet.range("A2").expand().options(pd.DataFrame).value
+        if data.empty:
+            print(f"No data found for ticker: {ticker}")
+            ineligible_tickers.append(ticker)
+            continue
+        # Rename the columns to the ticker symbol
+        data.rename(columns={data.columns[0]: ticker}, inplace=True)
+        stock_data[ticker] = data
+    wb.close()
+    app.quit()
+
+    sd = pd.DataFrame()
+
+    # Merge all the dataframes into one with the symbol as the column name
+    for symbol, data in stock_data.items():
+        data.index = pd.to_datetime(data.index)
+        data['Date'] = data.index
+        # Reindex the data to remove date as index
+        data.reset_index(drop=True, inplace=True)
+        if sd.empty:
+            sd = data
+        else:
+            sd = pd.merge(sd, data, on='Date', how='outer')
+    # Set the date as index
+    sd.set_index('Date', inplace=True)
+
+    # Delete columns with no data
+    sd.dropna(axis=1, how='all', inplace=True)
+
+    # Create a new column for every column
+    # in the dataframe and set the value to 0
+    # column name is column with suffix _weight
+    for col in sd.columns:
+        sd[col + '_weight'] = np.nan
+
+    return sd, ineligible_tickers
+
+def calculate_nav(stock_data, tradebook, ineligible_tickers=[]):
+    """
+    Function to calculate the Net Asset Value (NAV) of a personal fund based on stock data and tradebook.
+    """
+    if stock_data.empty or tradebook.empty:
+        print("Stock data or tradebook is empty. Cannot calculate NAV.")
+        return None
+
+    # Drop ineligible tickers from stock_data and tradebook
+    if ineligible_tickers:
+        stock_data = stock_data.drop(columns=[ticker for ticker in ineligible_tickers if ticker in stock_data.columns], errors='ignore')
+        tradebook = tradebook[~tradebook['ticker'].isin(ineligible_tickers)]
+
+    # Iterate through the df (tradebook) in order to get the weights
+    # for each stock on each date
+    # If stock is traded then the weight is increased by the quantity
+    # If stock is not traded then the weight is not changed
+    for index, row in tradebook.iterrows():
+        ticker = row['ticker']
+        quantity = row['quantity']
+        date = row['date']
+        # Add the quantity to the weight of the stock on that date if weight was not nan
+        # If weight was nan, then set it to the quantity
+        if pd.isna(stock_data.loc[date, ticker + '_weight']):
+            stock_data.loc[date, ticker + '_weight'] = quantity
+        else:
+            stock_data.loc[date, ticker + '_weight'] += quantity
+        # For all other tickers if there was a weight on previous date, then set it to previous weight
+        # If there was no previous weight, then set it to 0
+        # Use iloc to get the previous date. If its first row, then set it to 0
+        for col in stock_data.columns:
+            if col.endswith('_weight') and col != ticker + '_weight':
+                if pd.isna(stock_data.loc[date, col]):
+                    if date == stock_data.index[0]:
+                        stock_data.loc[date, col] = 0
+                    else:
+                        stock_data.loc[date, col] = stock_data.iloc[stock_data.index.get_loc(date) - 1][col]
+        # Fill forward the weights for nan values
+        stock_data.fillna(method='ffill', inplace=True)
+
+    price_cols =  stock_data.columns[~stock_data.columns.str.endswith('_weight')]
+
+    # aum is sumproduct of price and weight for each stock from pf
+    aum = []
+    for date, row in stock_data.iterrows():
+        aum_value = 0
+        for cols in price_cols:
+            weight_col = cols + '_weight'
+            aum_value += row[cols] * row[weight_col]
+        if aum_value == 0 and len(aum) > 0:
+            aum_value = aum[-1]
+        aum.append(aum_value)
+
+    initial_nav = 100
+    nav = [initial_nav]
+    units = [aum[0] / initial_nav]
+    result = pd.DataFrame({
+        'AUM': aum
+    }, index=stock_data.index)
+
+    for date, row in stock_data.iterrows():
+        # Skip the first date as it is the initial date
+        if date == stock_data.index[0]:
+            result.loc[date, 'Units'] = units[0]
+            result.loc[date, 'NAV'] = result.loc[date, 'AUM'] / units[0]
+            continue
+        prev_units = units[-1]
+        prev_nav = nav[-1]
+        if date in tradebook['date'].values:
+            # If the tradebook has a record for this date, add units
+
+            if tradebook.loc[date, 'quantity'] != 0:
+                units.append(prev_units + (tradebook.loc[date, 'quantity'] * tradebook.loc[date, 'price'] / prev_nav))
+            else:
+                units.append(prev_units)
+
+        # Append AUM/units to nav
+        nav.append(result.loc[date, 'AUM'] / units[-1])
+        # Add NAV and units to result dataframe for this date
+        result.loc[date, 'NAV'] = nav[-1]
+        result.loc[date, 'Units'] = units[-1]
+
+    # Merge the NAV with the stock data
+    final_result = pd.merge(result, stock_data, left_index=True, right_index=True, how='outer')
+
+    return final_result
 
 def construct_fund(broker, start_date=None, end_date=date.today()):
     """
@@ -152,20 +329,9 @@ def construct_fund(broker, start_date=None, end_date=date.today()):
 
 if __name__ == "__main__":
     # Example usage
-    fund = construct_fund('zerodha', start_date='2021-11-29', end_date='2023-09-11')
-    # Set display options to show the full DataFrame
-    pd.set_option('display.max_rows', None)  # Show all rows
-    pd.set_option('display.max_columns', None)  # Show all columns
-    pd.set_option('display.width', None)  # Adjust the width to fit the DataFrame
-    pd.set_option('display.max_colwidth', None)  # Show full content of each column
+    tradebook, start_date = get_tradebook('zerodha')
+    stock_data, iticks = get_stock_data(tradebook['ticker'].unique(), start_date=start_date)
+    nav_data = calculate_nav(stock_data, tradebook, iticks)
+    print(nav_data.head())
+    print(nav_data.info())
 
-    print(fund)
-
-    # Plot the NAV
-    plt.figure(figsize=(14, 7))
-    plt.plot(fund.index, fund['NAV'], label='NAV', color='blue')
-    plt.title('Net Asset Value (NAV) Over Time')
-    plt.xlabel('Date')
-    plt.ylabel('NAV')
-    plt.legend()
-    plt.show()
